5658e7b14ead976231de1299aab08daf30068ae2
MISTUser <MISTUser@lab8L4JUY>
2024-02-06 21:18:36 -0500

    Converted html into cshtml. Making this commit to have a point to go back on with BOTH formats.

---
+++ TruckingProject/Pages/RoutePlanner.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TruckingProject.Pages
+{
+    public class RoutePlannerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
